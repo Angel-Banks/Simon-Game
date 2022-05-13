268d275ac378eb36d26a64c5b325730eca2ea3ec
Angel-Banks <angelonuchuks@gmail.com>
2022-05-13 20:26:45 +0100

    updated

--- game.js
+++ game.js
@@ -1,3 +1,22 @@
 
+var buttonColors = ["red", "blue", "green", "yellow"];
 
-$ ('h1');
+
+
+var gamePattern = [];
+
+function nextSequence (){
+
+var randomNumber = Math.floor(Math.random() * 4);
+
+var randomChosenColor = buttonColors [randomNumber];
+
+gamePattern.push(randomChosenColor);
+
+$("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+};
+
+
+
+
+// buttonColors = [red, blue, gren, yellow]
